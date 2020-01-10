6a223f79c40efdb3d08b6669798833e6765c82d1
theqoobee <qoobeethegreat@gmail.com>
2020-01-11 00:25:30 +0100

    10 missing commits. Base app set up

---
+++ src/main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) { 
+  app.quit();
+}
+
+let mainWindow;
+
+const createWindow = () => {
+  
+  
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  mainWindow.on('closed', () => {
+
+    mainWindow = null;
+
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+
+  if (mainWindow === null) {
+    createWindow();
+  }
+
+});
