1f0c7a0ca2a8810f9bf65cbd52c9a3487296b2d1
theqoobee <qoobeethegreat@gmail.com>
2020-01-18 18:58:33 +0100

    main. js is mostly done yeey

--- src/main.js
+++ src/main.js
@@ -1,115 +1,105 @@
-'use strict';
+'use strict'
 
-const {
-    app,
-    BrowserWindow,
-    ipcMain
-} = require('electron');
-const path = require('path');
-const url = require('url');
-const DiscordRPC = require('discord-rpc');
-var init = false;
+// Including libraries
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+const url = require('url')
+// const rpc = require('./js/rpc.js');
+const isEqual = require('lodash.isequal')
 
-process.env.NODE_ENV = 'production';
+// Set the mode to development
+process.env.NODE_ENV = 'development'
 
-
+// This is to prevent the app from launching lots of times during
+// installation on windows
 if (require('electron-squirrel-startup')) {
-    app.quit();
+  app.quit()
 }
 
-let mainWindow;
+// Define the window
+
+let mainWindow
 
 const createWindow = () => {
+  mainWindow = new BrowserWindow({ // Setting the dimnesions
+    width: 800,
+    height: 450,
+    vibrancy: 'dark', // mac only
+    webPreferences: {
+      nodeIntegration: true // Make sure node is there
+    }
+  })
 
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 450,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+  mainWindow.openDevTools()
 
-    mainWindow.removeMenu();
+  mainWindow.removeMenu() // I don't need the menu there
 
-    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  // Load the correct file
+  mainWindow.loadFile(path.join(__dirname, 'index.html'))
 
-    mainWindow.on('closed', () => {
+  // Make sure the window is gone whne closed
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
 
-        mainWindow = null;
+// The function that creates the window
 
-    });
-};
+// When the app is loaded, create the new window
+app.on('ready', createWindow)
 
-app.on('ready', createWindow);
+// Instructions on what to do when the window is closed
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-});
+  // This is because mac allows all windows to be closed with
+  // the app still staying open
+  if (process.platform !== 'darwin') {
+    // app.quit(); // quit the app
+    createWindow()
+    rpc.destroy()
+  }
+})
 
 app.on('activate', () => {
+  // An extension of the code above
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+// ------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------
+// The above code was setup, now that that's done let's get down to the juicy parts
 
-    if (mainWindow === null) {
-        createWindow();
-    }
+// To elaborate how this works, the main.js function acts like an intermediary between renderer.js and rpc.js
+// So rpc.js checks for updates every 12 seconds, by seeing if the args object in the main functions changed
+// if it has rpc.js updates the rich presence, and if not, nothing changes
+// // when the user decides to change something in the rpcengine app, renderer beams the new info over to main.js
 
-});
+var changed = true // A variable to keep track of cahnged
 
-
-const clientId = '665244956805300274';
-DiscordRPC.register(clientId);
-
-const rpc = new DiscordRPC.Client({ transport: 'ipc' });
-
-
-const startTimestamp = new Date();
-
-async function setActivity() {
-    if (!rpc || !mainWindow) {
-        return;
-    }
+// These are the initial arguments
+var mainArgs = {
+  details: 'Usingadsasd rpcengine',
+  state: 'by theqoobee',
+  startTimestamp: true, // Timestamp to track the passage of time
+  largeImageKey: 'megu1', // name of the pics
+  smallImageKey: 'naruto_sleep', // ^^^
+  largeImageText: "I'm big image", // the text displayed when hovering over them
+  smallImageText: "I'm small image",
+  instance: false // base argument
 }
-ipcMain.on('simple', (event, args) => {
-    init = true;
- 
 
-    try {
-      if (args.timer){
-              rpc.setActivity({
-                  details: args.text1,
-                  state: args.text2,
-                  startTimestamp,
-                  largeImageKey: args.imagelg,
-                  smallImageKey: args.imagesm,
-                  instance: false,
-              });} else {
-                rpc.setActivity({
-                  details: args.text1,
-                  state: args.text2,
-                  largeImageKey: args.imagelg,
-                  smallImageKey: args.imagesm,
-                  instance: false,
-              });
-              }
-    } catch(err) {
-        event.returnValue = 'Something went wrong! ' + err.message;
-    }
-    event.returnValue =  { success: true, user: username };
+// This function reacts to a message from the renderer process, specifically looking at if anything changed
+// If anything changed, it give it to the rpc function wheen it checks
+ipcMain.on('change', (event, args) => {
+  if (!isEqual(mainArgs, args)) {
+    changed = true
+  }
+})
 
-});
-
-var username;
-rpc.on('ready', () => {
-username = rpc.user.username + "#" + rpc.user.discriminator;
-    
-    setActivity();
-    setInterval(() => {
-        setActivity();
-
-    }, 15e3);
-});
-
-rpc.login({
-    clientId
-}).catch(console.error);
+ipcMain.on('check', (event, args) => {
+  if (changed) {
+    event.returnValue = mainArgs
+    changed = false
+  } else { event.returnValue = false }
+})
