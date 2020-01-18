847fbc928aee11dc01d128651c5821ccbc43b876
theqoobee <qoobeethegreat@gmail.com>
2020-01-18 03:29:05 +0100

    Commented the living hell out of this file, easy to understand 100

--- src/js/rpc.js
+++ src/js/rpc.js
@@ -7,8 +7,8 @@ const ipcRPC = require('electron').ipcRenderer; // This is how we send messages 
 const timestamp = new Date(); // This is what we use for the timestamp
 var mainArgs, hasTime;
 // Default vars
-// just the example used in development
-var mainArgs = {
+
+var args = {
     details: "Using rpcengine",
     state: "by theqoobee",
     startTimestamp: true, // Timestamp to track the passage of time
@@ -19,41 +19,50 @@ var mainArgs = {
     instance: false,
 };
 
-var rpc = new DiscordRPC.Client({ transport: 'ipc' });
+var rpc = new DiscordRPC.Client({ transport: 'ipc' }); // setting up the richpresence object
 
+// The purpose of the following function is to check whether or not there are any updates to the arguments
+//
+// It sends a synchornous message to main via ipcRenderer, and as a result gets either false, meaning there 
+// were no changes, or a changed version of the arguments 
 
 async function check() {
-	var back = ipcRPC.sendSync('check', 'im checkin');
-	if (back) {
-		args = back;
+	let back = ipcRPC.sendSync('check', 'Any news BROTHA?'); // this is the sync function
+	if (back) { // If back is true, in other words if it isn't false, null, or undefined
+		args = back; // change the value of args to the value given by the main process
 	}
 }
 
-
+// The function to set the rich presence of the user
+//
 async function setActivity() {
-  if (!rpc) {
+  if (!rpc) { // if the rpc object was not properly initialized, do not run
     return;
   }
-
+  // here i chose to make a temporary version of the arguments for the sake of not changing
+  // the real ones when i set the timestamp there because the user turning them off would be a problem
   tempArgs = args; 
-  if (args.startTimestamp) {
-  	tempArgs.startTimestamp = timestamp;
-  } else { tempArgs.startTimestamp = null; }
+  if (args.startTimestamp) { // if the user chose to have a timestamp
+  	tempArgs.startTimestamp = timestamp; // set the field to a timestamp
+  } else { tempArgs.startTimestamp = null; } // else set it to null
 
-  rpc.setActivity(tempArgs).catch(console.error);
+  // send the activity to discord, if there are any errors resolve the failed promise
+  rpc.setActivity(tempArgs).catch(console.error); 
+
 
 }
 
+// finally, execute the setActivity() function only when rpc is ready
 
 rpc.on('ready', () => {
-  setActivity();
+  setActivity(); // execute the function
 
-  setInterval(() => {
-  	check();
-    setActivity();    
-  }, 10e3);
+  setInterval(() => { // set an interval of 10 seconds
+  	check(); // check if there's anything new, if so store the object
+    setActivity(); // set the activity
+  }, 10e3); // wait 10 seconds before repeating the same thing
 
 });
 
-
-rpc.login({clientId}).catch(console.error);
+// Login to finalize everything
+rpc.login({clientId}).catch(console.error); // again resolve failed promise
